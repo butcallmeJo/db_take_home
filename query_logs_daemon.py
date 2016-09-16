bbacafcbe9c892d3d7ce0088dcb54cb9a38b8d86
butcallmejo <josquin.gaillard@gmail.com>
2016-09-16 10:33:33 -0700

    advanced in project

--- query_logs_daemon.py
+++ query_logs_daemon.py
@@ -1,3 +1,19 @@
+'''
+Daemon that once every 10 seconds, writes out the average queries per 
+second per status code per route and total from a given log called 
+access.log.
+'''
+
 import daemon
 import time
 
+def get_info():
+
+def run():
+'''simple function to run the daemon'''
+    with daemon.DaemonContext():
+        # func
+
+
+if __name__ == "__main__":
+    run()
