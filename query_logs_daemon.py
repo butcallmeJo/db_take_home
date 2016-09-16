792a141c4a418e1040df4d45278d165089a9f0c8
butcallmejo <josquin.gaillard@gmail.com>
2016-09-16 13:45:59 -0700

    more progress. redirected output of daemon to stdout

--- query_logs_daemon.py
+++ query_logs_daemon.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 '''
 Daemon that once every 10 seconds, writes out the average queries per 
 second per status code per route and total from a given log called 
@@ -6,14 +8,29 @@ access.log.
 
 import daemon
 import time
+import sys
 
-def get_info():
+def get_info(log_path):
+    print "test info"
+    while True:
+        # print "test inf loop"
+        with open(log_path, 'r') as log:
+            print "test open log"
+            print log
+            print log.tell()
+        time.sleep(10)
 
-def run():
-'''simple function to run the daemon'''
-    with daemon.DaemonContext():
+def run_daemon(log_path):
+    '''simple function to run the daemon'''
+    print "test run"
+    # out = open('log_averages', 'w+')
+    with daemon.DaemonContext(stdout=sys.stdout):
         # func
+        sys.stdout.write("test run")
+        get_info(log_path)
 
 
 if __name__ == "__main__":
-    run()
+    print "test main"
+    log_path = sys.argv[1]
+    run_daemon(log_path)
