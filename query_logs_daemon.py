7a10e4c82da354e415a95e4390e75046dac71733
butcallmejo <josquin.gaillard@gmail.com>
2016-09-17 16:27:42 -0700

    finished main part

--- query_logs_daemon.py
+++ query_logs_daemon.py
@@ -13,7 +13,14 @@ import os
 import subprocess
 import select
 
+# Global variable for 10 sec worth of lines
 ten_sec_log = []
+
+# Global variable for organizing the list above
+ten_sec_data = {}
+
+# Format used for randomly generated log lines.
+OUTPUT_FORMAT = "{route}\t{status}\t{qps}"
 
 def get_info(log_path):
     global ten_sec_log
@@ -26,12 +33,44 @@ def get_info(log_path):
         if p.poll(1):
             ten_sec_log.append(log.stdout.readline(),)
 
+def print_report():
+    global ten_sec_data
+    global ten_sec_log
+    print time.strftime("%a %b\t%d %H:%M:%S %Y", time.localtime())
+    print "============================="
+    for key in ten_sec_data:
+        key_arr = key.split()
+        print OUTPUT_FORMAT.format(
+            route = key_arr[0],
+            status = key_arr[1],
+            qps = (ten_sec_data[key])/10.0
+        )
+    print "total\t" + str(len(ten_sec_log)) + "\n"
+
+def parse_log():
+    global ten_sec_log
+    global ten_sec_data
+    if not ten_sec_log:
+        return
+    for line in ten_sec_log:
+        line = line.split()
+        route = line[2]
+        status = line[3]
+        key = route + ' ' + status
+        if key in ten_sec_data:
+            ten_sec_data[key] += 1
+        else:
+            ten_sec_data[key] = 1
+    print_report()   
+    # print len(ten_sec_log)
+
 def manage_output():
     global ten_sec_log
+    global ten_sec_data
     threading.Timer(10, manage_output).start()
-    print ten_sec_log
-    # parse ten_sec_log here
-    # flush ten_sec_log here
+    parse_log()
+    ten_sec_log = []
+    ten_sec_data = {}   
 
 if __name__ == "__main__":
     log_path = "/home/jodag/Dev/personal/db_take_home/access.log" # put the path of the log here
