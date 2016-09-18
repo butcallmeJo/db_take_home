7892105f8cddc4321949627920895bc34793464d
butcallmejo <josquin.gaillard@gmail.com>
2016-09-18 14:52:48 -0700

    saving up before next big changes

--- query_logs_daemon.py
+++ query_logs_daemon.py
@@ -10,9 +10,8 @@ import threading
 import time
 import subprocess
 import select
-
-# Defining what log file to look at
-LOG_PATH = "/home/jodag/Dev/personal/db_take_home/access.log" # TODO
+import argparse
+import sys
 
 # Global variable for 10 sec worth of lines
 TEN_SEC_LOG = []
@@ -23,13 +22,13 @@ TEN_SEC_DATA = {}
 # Format used for outputting processed log data.
 OUTPUT_FORMAT = "{route}\t{status}\t{qps}" # qps is query per seconds
 
-# lock for threading
+# lock for threading - to not access TEN_SEC_LOG at the same time
 LOCK = threading.Lock()
 
-def get_info():
+def get_info(log_path):
     """Get every line from the log."""
     global TEN_SEC_LOG
-    log = subprocess.Popen(['tail', '-F', LOG_PATH], \
+    log = subprocess.Popen(['tail', '-F', log_path], \
         stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     p = select.poll()
     p.register(log.stdout)
@@ -85,9 +84,28 @@ def manage_output():
     parse_log()
     TEN_SEC_DATA = {}
 
-if __name__ == "__main__":
+def main(argv):
     """Main part of the program."""
+    # Parsing with argparse for the log file
+    parser = argparse.ArgumentParser(
+        description="Process a log in real time"
+    )
+    parser.add_argument(
+        "--input-file", "-i", dest="log_file", metavar='FILE',
+        type=str, required=True,
+        help=(
+            "the file where requests are logged. Full path preferred"
+        )
+    )
+    args = parser.parse_args(argv[1:])
+    log_path = args.log_file
     # start the manage_output function which will loop every 10 seconds
     manage_output()
     # start the get_info function that reads the log in constant time
-    get_info()
+    get_info(log_path)
+
+if __name__ == "__main__":
+    try:
+        main(sys.argv)
+    except KeyboardInterrupt:
+        sys.exit()
